f8b7968ad2fdd359769ffad97e168332a0728011
BILLIET Louis <lb3@dim-ub2.local>
2018-06-08 16:55:19 +0200

    RMooc temp1 facteur.R

--- Domino/R/RMooc/OpenClassRooms/facteurs.R
+++ Domino/R/RMooc/OpenClassRooms/facteurs.R
@@ -1,20 +1,37 @@
-rm(list=ls())
-chemRepTr<-getwd()
+rm(list = ls())
+chemRepTr <- getwd()
 chemRepTr
-resRchYaf<-grep("lb3", chemRepTr, ignore.case = T)
+resRchYaf <- grep("lb3", chemRepTr, ignore.case = T)
 resRchYaf
-resRchTourLin<-grep("/home/louis", chemRepTr, ignore.case = T)
+resRchTourLin <- grep("/home/louis", chemRepTr, ignore.case = T)
 resRchTourLin
-if (resRchYaf==1) {
+if (resRchYaf == 1) {
   setwd("C:/Users/lb3/OneDrive/sync/git/")
-} else if (resRchTourLin==1) {
+} else if (resRchTourLin == 1) {
   setwd("~/git/")
 }
 rm(chemRepTr, resRchTourLin, resRchYaf)
 getwd()
 setwd("loubill/Domino/R/RMooc/OpenClassRooms/")
 
-#######facteurs sous R = servent à stocker infos qualit, donc avec valeur descriptive
-#######facteurs, contrairement aux vecteurs, peuvent contenir des éléments de types différents
+####### facteurs sous R = servent à stocker infos qualit, donc avec valeur descriptive
+####### facteurs, contrairement aux vecteurs, peuvent contenir des éléments de types différents
 ####### un des attributs du fact = niveaux, qui est le vecteur qui contient lensemble des valeurs qui peuvent être prises pas les éléments (UNIVERS DES DONNEES)
-notes<-factor(c(14,19,16.5,13,17,5,10,3,13.5,12,8.75))
+notes <-
+  factor(c(14, 19, 16.5, 13, 17, 5, 10, 12, 8, 9, 10, 14, 3, 13.5, 12, 8.75))
+notes  #partie 'levels' renvoie l'univers des données ici, basé sur le contenu unitaire
+notes <- factor(levels = c(0:20))
+notes  #ici définition a priori univers des données de 0 à 20
+notes <-
+  factor(c(14, 19, 16, 13, 17, 5, 10, 12, 8, 9, 10, 14, 3, 13.5, 12, 8.75),
+         levels = c(0:20))
+notes  #suppr des valeurs aberrantes en conflit avec univers des données pour les passer en NA
+names(notes) <- c("DM1", "DM2", "DS1", "DM3", "DS2", "DS3", "DS4")
+summary(notes)
+notes
+notes <-
+  factor(c(14, 19, 16, 13, 17, 5, 10, 12, 8, 9, 10, 14, 3, 13.5, 12, 8.75),
+         levels = seq(0,20,0.25))
+notes
+summary(notes)
+table(notes)
