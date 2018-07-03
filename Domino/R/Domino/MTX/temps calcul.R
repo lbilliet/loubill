0fc284bd170d82decdc10b0f573dc66a77a5e139
Louis Billiet <louis.bllt@gmail.com>
2018-07-03 14:08:56 +0200

    réunion 20180702 Seb mclustcomp remis en qst

--- Domino/R/Domino/MTX/temps calcul.R
+++ Domino/R/Domino/MTX/temps calcul.R
@@ -1,7 +1,9 @@
-Rprof(NULL)
 # setwd("C:/Users/lb3/OneDrive/sync/git/loubill/Domino/R/Domino/MTX")
-setwd("F:/OneDrive/sync/git/loubill/Domino/R/Domino/MTX")
+# setwd("F:/OneDrive/sync/git/loubill/Domino/R/Domino/MTX")
+setwd("/media/louis/EXCHWINLIN/Gdrive/sync/git/loubill/Domino/R/Domino/MTX")
 # install.packages(c('tm', 'SnowballC', 'wordcloud', 'topicmodels', "koRpus", "mclustcomp", "devtools", "installr", "RNewsflow", "koRpus","wordVectors"))
+install.packages("tm")
+install.packages("libxml2-dev")
 # installr::install.Rtools()
 # devtools::install_github("lbilliet/loubill")
 # slam_url <- "https://cran.r-project.org/src/contrib/Archive/slam/slam_0.1-37.tar.gz"
@@ -10,17 +12,17 @@ setwd("F:/OneDrive/sync/git/loubill/Domino/R/Domino/MTX")
 # install_url(urlSlam)
 # install_github("bmschmidt/wordVectors")
 #devtools::install_github("bmschmidt/wordVectors")
-Rprof(append = TRUE, filename = "tps/Rprof.out")
 library('devtools')
-# library(tm)
-# library(SnowballC)
+library(tm)
+library(SnowballC)
 # library(wordcloud)
 # library(topicmodels)
 library("koRpus")
 # library("wordVectors")
-library("RNewsflow")
+# library("RNewsflow")
 library("mclustcomp")
 
+system.time(expr={
 ######import données
 df.mtx<-read.csv2("data/mtx.csv", sep = "\n" ,header=F, quote = "", stringsAsFactors = F, fileEncoding = "UTF8")
 df.alt<-read.csv2("data/alt.csv", sep = "\n" ,header=F, quote = "", stringsAsFactors = F, fileEncoding = "UTF8")
@@ -36,15 +38,12 @@ tdm.alt <- TermDocumentMatrix(corpus.alt)
 #matrice frequence documents
 dtm.mtx <- DocumentTermMatrix(corpus.mtx, control = list(weighting = weightTfIdf, stopwords = TRUE))
 dtm.alt <- DocumentTermMatrix(corpus.alt, control = list(weighting = weightTfIdf, stopwords = TRUE))
-#démo rch termes retrouvés à une fréq de 2
-freq.tdm.mtx<-findFreqTerms(tdm.mtx,lowfreq = 5, highfreq = Inf)
-freq.tdm.alt<-findFreqTerms(tdm.alt,lowfreq = 5, highfreq = Inf)
 
 ################################
 #boucle constitution vecteur concaténation corpus mtx
 txt.mtx<-NULL
 for (i in 1 : nrow(df.mtx)){
-  txt.mtx<-paste0(df.mtx[i,])
+  txt.mtx<-paste0(txt.mtx,df.mtx[i,])
 }
 
 vec.mtx<-unlist(strsplit(txt.mtx, split = " "))
@@ -56,6 +55,9 @@ write.table(vec.mtx, file = "data/stemVecAlt.txt", sep = "\t", row.names = F, qu
 write.table(vec.mtx, file = "data/stemVecMtx.txt", sep = "\t", row.names = F, quote = F)
 
 #comparaison via mclustcomp
+vecteur1 <- c(0:10)
+vecteur2 <- c(11:1)
+debug(mclustcomp)
+mclustcomp(vecteur1,vecteur2,types="all")
 mclustcomp(vec.mtx,vec.mtx, types = "all")#pb ici il faut une boucle pour parcourir chaque vecteur de la df
-Rprof(NULL)
-summaryRprof(filename = "tps/Rprof.out")
+})
