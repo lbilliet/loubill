7aeee44c190cc97779ff485a6cebd22cbe2d23eb
lbillietYAF <louis.bllt@gmail.com>
2018-07-06 16:25:48 +0200

    Domino R vendr quanteda tuto dist

--- Domino/R/MTX/Text Analysis with R for Students of Literature.R
+++ Domino/R/MTX/Text Analysis with R for Students of Literature.R
@@ -14,11 +14,14 @@ vec2.mtx<-scan(file = "data/mtx2.csv", sep="\t", what = character(), quote = "")
 vec3.mtx<-scan(file = "data/mtx3.csv", sep="\t", what = character(), quote = "")
 vec.concat.mtx<-c(vec1.mtx, vec2.mtx, vec3.mtx)
 summary(vec.concat.mtx)
+vec.concat.mtx
 summary(vec.mtx)#pourquoi 50 ici ?
 
 #reprocessing tokens
 tok.mtx<-tokens(vec.concat.mtx) 
-dfm.mtx<-dfm(tok.mtx, remove_punct=TRUE)
+dfm.mtx<-dfm(tok.mtx, remove_punct=TRUE, remove_numbers = TRUE, tolower = TRUE, verbose = TRUE)
+dfm.mtx
+dfm.mtx <- dfm_remove(dfm.mtx, "[0-9]", valuetype = "regex", verbose = TRUE)
 dfm.mtx
 ntoken(dfm.mtx)
 textstat_frequency(dfm.mtx, n=15)
@@ -32,3 +35,9 @@ head(relPct.MostFreqTerms.mtx)
 pct.dfm.mtx<-dfm_weight(dfm.mtx, scheme = "prop")*100
 summary(pct.dfm.mtx)
 pct.dfm.mtx[1:10,1:10]
+
+####cosine sim
+dist<-textstat_simil(dfm.mtx, margin="documents", method="cosine")
+head(dist)
+print(dist)
+as.data.frame(as.matrix(dist))
