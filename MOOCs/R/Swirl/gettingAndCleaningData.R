188e898b38fa379c7eb251fc3c08df4e9059e140
lbillietYAF <louis.bllt@gmail.com>
2018-07-24 17:37:40 +0200

    R mooc swirl getting data finished

--- MOOCs/R/Swirl/gettingAndCleaningData.R
+++ MOOCs/R/Swirl/gettingAndCleaningData.R
@@ -5,6 +5,9 @@ rm(list = ls())
 library(swirl)
 
 swirl()
+Louis
+3
+1
 mydf<-read.csv(path2csv, stringsAsFactors = FALSE)
 dim(mydf)
 str(mydf)
@@ -32,5 +35,23 @@ filter(cran, r_version == "3.1.1", country == "US")
 filter(cran, r_version <= "3.0.2", country == "IN")
 filter(cran, country == "US" | country == "IN")
 filter(cran, size > 100500, r_os == "linux-gnu")
-is.na(cran$r_version)
+is.na(c(3, 5, NA, 10))
+!is.na(c(3, 5, NA, 10))
 filter(cran, !is.na(r_version))
+
+cran2<-select(cran, size:ip_id)
+arrange(cran2,ip_id)
+arrange(cran2,desc(ip_id))
+arrange(cran2, package, ip_id)
+arrange(cran2, country, desc(r_version), ip_id)
+cran3<-select(cran, ip_id, package, size)
+cran3
+
+mutate(cran3, size_mb = size / 2^20)
+
+mutate(cran3, size_mb = size / 2^20, size_gb = size_mb / 2^10)
+mutate(cran3, correct_size = size +1000 )
+summarize(cran, avg_bytes=mean(size))
+
+1
+louis.bllt@gmail.com
